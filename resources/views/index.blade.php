70c95f3154dbfb363e5358cdb880e01d6d8e6b80
Saman Deshar <samandeshar01@gmail.com>
2025-02-05 00:16:56 +0545

    blog intial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,18 +8,35 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <h3>All Posts</h3>
+                <h3 class="mb-4">All Posts</h3>
 
                 @if ($posts->isEmpty())
-                    <p>No posts available.</p>
+                    <div class="alert alert-warning" role="alert">
+                        No posts available.
+                    </div>
                 @else
-                    <ul class="list-group">
+                    <div class="row">
                         @foreach ($posts as $post)
-                            <li class="list-group-item">
-                                <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-                            </li>
+                            <div class="col-md-4 mb-4">
+                                <!-- Post Block -->
+                                <div class="card h-100">
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="{{ route('posts.show', $post->id) }}" class="text-dark">
+                                                {{ $post->title }}
+                                            </a>
+                                        </h5>
+                                        <p class="card-text">
+                                            {{ Str::limit($post->content, 100) }} <!-- Displaying truncated content -->
+                                        </p>
+                                    </div>
+                                    <div class="card-footer text-muted">
+                                        <small>Posted on {{ $post->created_at->format('M d, Y') }}</small>
+                                    </div>
+                                </div>
+                            </div>
                         @endforeach
-                    </ul>
+                    </div>
                 @endif
             </div>
         </div>
